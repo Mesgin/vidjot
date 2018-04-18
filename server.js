6243f7a2f0d04940606d3f7fc98eb3b70d998308
Mo Mesgin <mo@Mos-MacBook-Pro.local>
2018-04-18 00:12:17 -0700

    setup express-handlebars

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 const express = require('express')
 const app = express()
+const exphbs = require('express-handlebars')
+
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+  }))
+app.set('view engine', 'handlebars')
 
 app.get('/',(req,res)=>{
     res.json()
