54f6f7945d6cb594484f5cf084cf6eeb34e3520f
Mo Mesgin <mo@Mos-MacBook-Pro.local>
2018-04-18 22:30:05 -0700

    setup idea model

--- server.js
+++ server.js
@@ -1,31 +1,47 @@
 const express = require('express')
-const app = express()
-const exphbs = require('express-handlebars')
+const exphbs  = require('express-handlebars')
 const mongoose = require('mongoose')
 
-mongoose.Promise = global.Promise 
-mongoose.connect('mongodb//localhost/vidjot-db',{
-    useMongoClient:true
-})
-.then(()=> console.log('Mongodb is connected'))
-.catch(err => console.log(err))
+const app = express()
 
+// Map global promise - get rid of warning
+mongoose.Promise = global.Promise
+// Connect to mongoose
+mongoose.connect('mongodb://localhost/vidjot-dev')
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err))
 
+// Load Idea Model
+require('./models/Idea')
+const Idea = mongoose.model('ideas')
+
+// Handlebars Middleware
 app.engine('handlebars', exphbs({
-    defaultLayout: 'main'
-  }))
+  defaultLayout: 'main'
+}))
 app.set('view engine', 'handlebars')
 
-app.get('/',(req,res)=>{
-    const title = 'Welcome1'
-    res.render('index',{
-        title
-    })
-})
-app.get('/about',(req,res)=>{
-    res.render('about')
+
+// Index Route
+app.get('/', (req, res) => {
+  const title = 'Welcome'
+  res.render('index', {
+    title: title
+  })
 })
 
-app.listen(8080,()=>{
-    console.log(8080)
+// About Route
+app.get('/about', (req, res) => {
+  res.render('about')
+})
+
+// Add Idea Form
+app.get('/ideas/add', (req, res) => {
+  res.render('ideas/add')
+})
+
+const port = 5000
+
+app.listen(port, () =>{
+  console.log(`Server started on port ${port}`)
 })
