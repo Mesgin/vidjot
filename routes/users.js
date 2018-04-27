0d3f9a8bee1b897b9a9e5396fd9cf475f8bed213
Mo Mesgin <mo.mesgin@gmail.com>
2018-04-26 22:18:56 -0700

    register save to db

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const router = express.Router()
 const mongoose = require('mongoose')
+//const passport = require('passport')
+const bcrypt = require('bcryptjs')
+
+// Load user model
+require('../models/User')
+const User = mongoose.model('users')
 
 // User login route
 router.get('/login', (req, res) => {
@@ -16,10 +22,10 @@ router.get('/register', (req, res) => {
 router.post('/register', (req, res) => {
     let errors = []
     if (req.body.password != req.body.password2) {
-        errors.push({text:'Passwords do not match'});
+        errors.push({ text: 'Passwords do not match' })
     }
     if (req.body.password.length < 8) {
-        errors.push({text:'Password length should be atleast 8 charactors'});
+        errors.push({ text: 'Password length should be atleast 8 charactors' })
     }
     if (errors.length > 0) {
         res.render('users/register', {
@@ -28,9 +34,37 @@ router.post('/register', (req, res) => {
             email: req.body.email,
             password: req.body.password,
             password2: req.body.password2
-        });
+        })
     } else {
-        res.send('passed')
+        User.findOne({ email: req.body.email })
+            .then(user => {
+                if (user) {
+                    req.flash('error_msg', 'Email already registered')
+                    res.redirect('/users/register')
+                } else {
+                    const newUser = new User({
+                        name: req.body.name,
+                        email: req.body.email,
+                        password: req.body.password
+                    })
+
+                    bcrypt.genSalt(10, (err, salt) => {
+                        bcrypt.hash(newUser.password, salt, (err, hash) => {
+                            if (err) throw err
+                            newUser.password = hash
+                            newUser.save()
+                                .then(user => {
+                                    req.flash('success_msg', 'You are now registered and can log in')
+                                    res.redirect('/users/login')
+                                })
+                                .catch(err => {
+                                    console.log(err)
+                                    return
+                                })
+                        })
+                    })
+                }
+            })
     }
 })
 
