3f44b5bd9dcf4ae67d284c033576269de9e9fde5
Mo Mesgin <mo.mesgin@gmail.com>
2018-04-27 12:14:44 -0700

    fix issue with toggling navbar links

--- config/passport.js
+++ config/passport.js
@@ -9,7 +9,7 @@ module.exports = function(passport){
   passport.use(new LocalStrategy({usernameField: 'email'},(email,password,done)=>{
     // Match user
     User.findOne({
-      email
+      email:email
     }).then(user => {
       if(!user){
         return done(null,false,{message: 'No user found'})
@@ -27,12 +27,13 @@ module.exports = function(passport){
     })
   }))
 
-  passport.serializeUser(function(user,done){
-    done(null,user.id)
+  passport.serializeUser(function(user, done) {
+    done(null, user.id)
   })
-  passport.deserializeUser(function(id,done){
-    User.findById(id,function(err,user){
-      done(err,user)
+  
+  passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+      done(err, user)
     })
   })
 }
